6ac11b53ee5cc6496eb5522f42f91687887a4387
Sainath More <sainathmore501@gmail.com>
2025-07-02 12:36:25 +0000

    Initial commit

---
+++ src/BasicNft.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "";
+}
